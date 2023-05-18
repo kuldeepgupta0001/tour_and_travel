e880d4e9db92e9a317966c496b014a00505638df
Kuldeep Kumar Gupta <guptakul0001@gmail.com>
2023-05-18 19:57:01 +0530

    tourandtravel

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from "./component/Header";
+import Home from "./component/Home";
+import Main from "./component/Main";
+import Footer from "./component/Footer";
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+      <Main />
+      {/* <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
