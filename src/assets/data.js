239cf6e04c1cea1cd4cb45e7680e23e4b7233071
Kuldeep Kumar Gupta <guptakul0001@gmail.com>
2023-05-30 00:46:49 +0530

    update

---
+++ src/assets/data.js
@@ -0,0 +1,115 @@
+import img1 from "./img1.jpg";
+import img2 from "./img2.jpg";
+import img3 from "./img3.jpg";
+import img4 from "./img4.jpg";
+import img5 from "./img5.jpg";
+import img6 from "./img6.jpg";
+import img7 from "./img7.jpg";
+import img8 from "./img8.jpg";
+import img9 from "./img9.jpg";
+import img10 from "./img10.jpg";
+
+const Data = [
+  {
+    id: 1,
+    imgSrc: img1,
+    destTitle: "Bali Island",
+    location: "Indonesia",
+    garde: "CULTURAL RELAX",
+    fees: "$500",
+    description:
+      "Bali is an Indonesia Island and one of the Best holiday destination in the Indonesia archipelago",
+  },
+  {
+    id: 2,
+    imgSrc: img2,
+    destTitle: "Bali Island",
+    location: "Indonesia",
+    garde: "CULTURAL RELAX",
+    fees: "$500",
+    description:
+      "Bali is an Indonesia Island and one of the Best holiday destination in the Indonesia archipelago",
+  },
+  {
+    id: 3,
+    imgSrc: img3,
+    destTitle: "Bali Island",
+    location: "Indonesia",
+    garde: "CULTURAL RELAX",
+    fees: "$500",
+    description:
+      "Bali is an Indonesia Island and one of the Best holiday destination in the Indonesia archipelago",
+  },
+  {
+    id: 4,
+    imgSrc: img4,
+    destTitle: "Bali Island",
+    location: "Indonesia",
+    garde: "CULTURAL RELAX",
+    fees: "$500",
+    description:
+      "Bali is an Indonesia Island and one of the Best holiday destination in the Indonesia archipelago",
+  },
+  {
+    id: 5,
+    imgSrc: img5,
+    destTitle: "Bali Island",
+    location: "Indonesia",
+    garde: "CULTURAL RELAX",
+    fees: "$500",
+    description:
+      "Bali is an Indonesia Island and one of the Best holiday destination in the Indonesia archipelago",
+  },
+  {
+    id: 6,
+    imgSrc: img6,
+    destTitle: "Bali Island",
+    location: "Indonesia",
+    garde: "CULTURAL RELAX",
+    fees: "$500",
+    description:
+      "Bali is an Indonesia Island and one of the Best holiday destination in the Indonesia archipelago",
+  },
+  {
+    id: 7,
+    imgSrc: img7,
+    destTitle: "Bali Island",
+    location: "Indonesia",
+    garde: "CULTURAL RELAX",
+    fees: "$500",
+    description:
+      "Bali is an Indonesia Island and one of the Best holiday destination in the Indonesia archipelago",
+  },
+  {
+    id: 8,
+    imgSrc: img8,
+    destTitle: "Bali Island",
+    location: "Indonesia",
+    garde: "CULTURAL RELAX",
+    fees: "$500",
+    description:
+      "Bali is an Indonesia Island and one of the Best holiday destination in the Indonesia archipelago",
+  },
+  {
+    id: 9,
+    imgSrc: img9,
+    destTitle: "Bali Island",
+    location: "Indonesia",
+    garde: "CULTURAL RELAX",
+    fees: "$500",
+    description:
+      "Bali is an Indonesia Island and one of the Best holiday destination in the Indonesia archipelago",
+  },
+  {
+    id: 10,
+    imgSrc: img10,
+    destTitle: "Bali Island",
+    location: "Indonesia",
+    garde: "CULTURAL RELAX",
+    fees: "$500",
+    description:
+      "Bali is an Indonesia Island and one of the Best holiday destination in the Indonesia archipelago",
+  },
+];
+
+export default Data;
--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -1,7 +1,57 @@
 import React from "react";
+import {
+  HiOutlineClipboardCheck,
+  HiOutlineLocationMarker,
+} from "react-icons/hi";
+
+import Data from "../assets/data.js";
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <section className="main container section">
+      <div className="secTitle">
+        <h3 className="title">Most visited destinations</h3>
+      </div>
+
+      <div className="secContent grid">
+        {Data.map(
+          ({ id, imgSrc, destTitle, location, garde, description, fees }) => {
+            return (
+              <div key={id} className="singleDestination">
+                <div className="imageDiv">
+                  <img src={imgSrc} alt={destTitle} />
+                </div>
+                <div className="cardInfo">
+                  <h4 className="destTitle">{destTitle}</h4>
+                  <span className="continent flex">
+                    <HiOutlineLocationMarker className="icon" />
+                    <span className="name">{location}</span>
+                  </span>
+                  <div className="fees flex">
+                    <div className="grade">
+                      <span>
+                        {garde}
+                        <small>+1</small>
+                      </span>
+                    </div>
+                    <div className="price">
+                      <h5>{fees}</h5>
+                    </div>
+                  </div>
+                  <div className="desc">
+                    <p>{description}</p>
+                  </div>
+                  <button className="btn flex">
+                    DETAILS <HiOutlineClipboardCheck className="icon" />
+                  </button>
+                </div>
+              </div>
+            );
+          }
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default Main;
