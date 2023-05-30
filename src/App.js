746689c413f7fb851da85d97a39d5419b8cfd768
Kuldeep Kumar Gupta <guptakul0001@gmail.com>
2023-05-30 15:36:46 +0530

    update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <Header />
       <Home />
       <Main />
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import video2 from "../assets/video2.mp4";
+import { FiSend } from "react-icons/fi";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <section className="footer">
+      <div className="videoDiv">
+        <video src={video2} loop autoPlay muted type="video/mp4"></video>
+      </div>
+
+      <div className="secContent container">
+        <div className="contactDiv flex">
+          <div className="text">
+            <small>KEEP IN TOUCH</small>
+            <h2>Travel with us</h2>
+          </div>
+
+          <div className="inputDiv flex">
+            <input type="text" placeholder="Enter Email Address" />
+            <button className="btn flex" type="submit">
+              SEND <FiSend className="icon" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Footer;
